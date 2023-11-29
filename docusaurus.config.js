2d7049ef049ca071f00840c75a1b7ba1420384e8
ZTRdiamond <95547694+ZTRdiamond@users.noreply.github.com>
2023-11-30 00:55:40 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,16 +38,10 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
+          sidebarPath: require.resolve("./sidebars.js")
         },
         blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
+          showReadingTime: true
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
